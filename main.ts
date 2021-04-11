5776d440d10cc6478dea18d1c9080f06332e5a25
H2msbeep <81969058+H2msbeep@users.noreply.github.com>
2021-04-12 06:52:38 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    if (!(input.logoIsPressed())) {
+        if (list[Itunfan] == 1) {
+            text_list[0] = "E"
+            Itunfan += 1
+            if (list[Itunfan] == 1) {
+                text_list[0] = "E"
+                Itunfan += 1
+                if (true) {
+                	
+                } else {
+                	
+                }
+            } else {
+            	
+            }
+        } else if (false) {
+        	
+        }
+    }
+})
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     led.plot(2, 2)
     music.playTone(262, music.beat(BeatFraction.Whole))
@@ -14,18 +35,11 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
     led.unplot(1, 2)
     led.unplot(2, 2)
     led.unplot(3, 2)
-    if (!(input.logoIsPressed())) {
-        if (list[0] == 1) {
-            text_list[i - 1] = "E"
-        } else if (list[0] == 2) {
-            text_list[i - 1] = "T"
-        }
-        basic.showString("" + (text_list[0]))
-    }
 })
 input.onLogoEvent(TouchButtonEvent.Released, function () {
 	
 })
+let Itunfan = 0
 let text_list: string[] = []
 let list: number[] = []
 let i = 0
