d73a5d70c9031dff34e418a1d2f3b6726d5b6f1d
H2msbeep <81969058+H2msbeep@users.noreply.github.com>
2021-04-12 21:33:54 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,29 +12,18 @@ input.onButtonPressed(Button.B, function () {
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.clearScreen()
+    list[i] = 1
     led.plot(2, 2)
     music.ringTone(988)
-    list[i] = 1
-    basic.pause(100)
-    if (input.logoIsPressed()) {
-        led.plot(1, 2)
-        led.plot(2, 2)
-        led.plot(3, 2)
-        list[i] = 2
-    }
 })
 input.onLogoEvent(TouchButtonEvent.Released, function () {
-    if (!(input.logoIsPressed())) {
-        i += 1
-        led.unplot(1, 2)
-        led.unplot(2, 2)
-        led.unplot(3, 2)
-        basic.pause(1000)
-        basic.showIcon(IconNames.Square)
-        list[i] = 0
-        i += 1
-        music.rest(music.beat(BeatFraction.Sixteenth))
-    }
+    basic.clearScreen()
+    i += 1
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.pause(500)
+    basic.showIcon(IconNames.Square)
+    list[i] = 0
+    i += 1
 })
 let asdf = 0
 let list: number[] = []
